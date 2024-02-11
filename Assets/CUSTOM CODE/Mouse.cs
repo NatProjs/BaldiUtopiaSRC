98f30f9b59cb1958b9c6f6ee09f7ae362df7e1a2
Nat <135191900+natstackzzz@users.noreply.github.com>
2024-02-11 10:19:30 -0500

    wip

---
+++ Assets/CUSTOM CODE/Mouse.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Assembly-CSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,2 @@
+﻿//  To the person who for some reason opened this file.
+// Did you know that I have a big-
